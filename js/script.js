3c1e52c8ff3753b5d486324d0bc072b0f66734e1
Philip Fol <philip.fol08@gmail.com>
2019-12-13 13:51:17 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var element1 = document.getElementById("break-increment")
+
+element1.addEventListener("click", function(){
+  element1.style.color = "blue";
+});
+
+var element2 = document.getElementById("break-decrement")
+
+element2.addEventListener("click", function(){
+  element2.style.color = "blue";
+});
+
+var element3 = document.getElementById("session-increment")
+
+element3.addEventListener("click", function(){
+  element3.style.color = "blue";
+});
+
+var element4 = document.getElementById("session-decrement")
+
+element4.addEventListener("click", function(){
+  element4.style.color = "blue";
+});
